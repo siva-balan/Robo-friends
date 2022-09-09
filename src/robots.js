84872ada10d08e80086f6b70e216b1753cc3e886
Sivabalan <sivabalan.arun@gmail.com>
2022-09-09 19:43:22 +0530

    First commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Siva',
+        username: 'Siv',
+        email: 'sivabalan@gmail.com'
+    },
+    {
+        id:2,
+        name: 'Arun',
+        username: 'Arun',
+        email: 'arun@gmail.com' 
+    },
+    {
+        id:3,
+        name: 'naveen',
+        username: 'nav',
+        email: 'nav@gmail.com'
+    },
+    {
+        id:4,
+        name: 'sethu',
+        username: 'Set',
+        email: 'sethu@gmail.com'
+    }
+];
