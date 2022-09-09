b161f321062f12e1ca0f7fc6070bee69bea17f64
Sivabalan <sivabalan.arun@gmail.com>
2022-09-09 23:11:21 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import Cardlist from "./Cardlist";
+import {robots} from "./robots";
+import Searchbox from "./Searchbox";
+
+class App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            robots : robots,
+            searchfield: ''
+        }
+    }
+
+    onsearch = (event) =>  {
+        this.setState({ searchfield: event.target.value })
+        
+    }
+
+    render () {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <Searchbox searchchange = {this.onsearch}/>
+                <Cardlist robots = {filteredRobots} />
+            </div>
+        );
+    }
+    
+}
+
+export default App;
--- src/robots.js
+++ src/robots.js
@@ -22,5 +22,41 @@ export const robots = [
         name: 'sethu',
         username: 'Set',
         email: 'sethu@gmail.com'
-    }
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
+      },
+      {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info'
+      },
+      {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz'
+      },
+      {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
+      },
+      {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io'
+      },
+      {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz'
+      }
 ];
